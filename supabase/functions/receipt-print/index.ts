f7d22875216d94ae174d72c78d5c22c23f2bb2e3
Bianca <biancacmatira@gmail.com>
2026-06-16 21:28:13 -0700

    createClient

--- supabase/functions/receipt-print/index.ts
+++ supabase/functions/receipt-print/index.ts
@@ -1,4 +1,7 @@
-import { createClient } from 'https://esm.sh'
+//import { createClient } from 'https://esm.sh'
+//import { createClient } from 'https://skypack.dev'
+import { createClient } from 'npm:@supabase/supabase-js@2'
+
 
 // — Constants
 const CORS = {
